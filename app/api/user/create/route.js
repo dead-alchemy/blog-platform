105099e5eae21e7446f7869b07d5faec6347e553
dead-alchemy <dlonicholas@gmail.com>
2023-09-18 21:23:57 -0600

    setting return codes for invalid forms.

--- app/api/user/create/route.js
+++ app/api/user/create/route.js
@@ -1,15 +1,32 @@
-import { NextResponse } from "next/server";
 import { signupSchema } from "@/lib/schemas";
-import { query } from "@/lib/pg";
+import { query, querySingle } from "@/lib/pg";
+import { validateURL } from "@/lib/functions";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
 	//return NextResponse.json("hello");
-	const parsedUrl = new URL(req.headers.get("referer"));
+	const requestURL = req.headers.get("referer");
+
+	if (!validateURL(requestURL)) {
+		return NextResponse.json(
+			{ message: "Not Authorizedasdf" },
+			{
+				status: 401,
+			}
+		);
+	}
 
 	const body = await req.json();
 
+	const parsedUrl = new URL(requestURL);
+
 	if (parsedUrl.pathname.toLowerCase() !== "/signup") {
-		return NextResponse.status(401).json("Not Authorized");
+		return NextResponse.json(
+			{ message: "Not Authorized" },
+			{
+				status: 401,
+			}
+		);
 	}
 
 	const validation = await signupSchema
@@ -24,7 +41,12 @@ export async function POST(req) {
 		});
 
 	if (validation !== null) {
-		return NextResponse.status(400).json(err);
+		return NextResponse.json(
+			{ message: JSON.stringify(err) },
+			{
+				status: 400,
+			}
+		);
 	}
 
 	//email
@@ -32,6 +54,22 @@ export async function POST(req) {
 	// last_name;
 	// password;
 	// birth_date;
+
+	const email_check = await querySingle(
+		`
+	select case count(*) 
+		when 0 then true
+		else false
+		end as email_check
+	from  blogplatform.public.users
+	where email_address = $1
+	`,
+		[body.email]
+	);
+
+	if (email_check === false) {
+		NextResponse.json({ message: "Email already in use" }, { status: 401 });
+	}
 
 	const result = await query("select $1, $2, $3, $4, $5", [
 		body.email,
