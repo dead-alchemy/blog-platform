d5301fc80f57243c0245c92774718427800c1ce3
dead-alchemy <dlonicholas@gmail.com>
2023-11-29 21:58:15 -0700

    updating compose and next config.

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "standalone",
+	webpackDevMiddleware: (config) => {
+		// Solve compiling problem via vagrant
+		config.watchOptions = {
+			poll: 1000, // Check for changes every second
+			aggregateTimeout: 300, // delay before rebuilding
+		};
+		return config;
+	},
 };
 
 module.exports = nextConfig;
