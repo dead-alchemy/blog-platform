1832a4e7737a84a54b750948b506314e920f1a0a
dead-alchemy <dlonicholas@gmail.com>
2023-12-01 14:03:56 -0700

    fixing function

--- client/lib/functions/index.js
+++ client/lib/functions/index.js
@@ -1,3 +1,3 @@
 export { validateURL as validateURL } from "./validateURL";
 export { checkAuth as checkAuth } from "./checkAuth";
-export { readCheckauth as readCheckauth } from "./readCheckAuth";
+export { readCheckAuth as readCheckAuth } from "./readCheckAuth";
